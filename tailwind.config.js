ae5b012b3644adcff340bcc88d2236d43106e817
priyanshu mehta <mehtapriyanshu1028@gmail.com>
2024-09-01 00:21:17 +0530

    some setup

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Adjust this path if your components are in different folders
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
